9b93e3abe8d2bfe417004524da334d4ef9019bdb
grant.derderian@usc.edu <gderderi@usc.edu>
2013-01-19 13:46:26 -0800

    Initial Commit

---
+++ Jarrive/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Jarrive
+//
+//  Created by Grant Derderian on 1/19/13.
+//  Copyright (c) 2013 com.frc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
