b7240a5607e394d5f8fc93847870781232ba6697
ilater <later@usc.edu>
2013-03-21 09:35:36 -0700

    added JarriveFriendTableViewController as a tableview controller file with NSMutableArray: friends as only property so far

---
+++ Jarrive/FriendTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  FriendTableViewController.h
+//  Jarrive
+//
+//  Created by Ian Later on 3/21/13.
+//  Copyright (c) 2013 com.frc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriendTableViewController : UITableViewController
+{
+    NSMutableArray* _friends;
+}
+
+@property NSMutableArray* friends;
+@end
