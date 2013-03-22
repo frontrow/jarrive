e90251fc9f81685f76044b5316177952cfc3f956
Kenneth Barry <kennetcb@usc.edu>
2013-03-22 11:32:51 -0700

    Added arrival

---
+++ Jarrive/Arrival/ArrivalManager.h
@@ -0,0 +1,13 @@
+//
+//  ArrivalManager.h
+//  Jarrive
+//
+//  Created by Clark Barry on 3/22/13.
+//  Copyright (c) 2013 com.frc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ArrivalManager : NSObject
+
+@end
---
+++ Jarrive/Arrival/ArrivalManager.m
@@ -0,0 +1,13 @@
+//
+//  ArrivalManager.m
+//  Jarrive
+//
+//  Created by Clark Barry on 3/22/13.
+//  Copyright (c) 2013 com.frc. All rights reserved.
+//
+
+#import "ArrivalManager.h"
+
+@implementation ArrivalManager
+
+@end
